8eb220231b9cb26329f1b051bc65c11d8c48c5f2
Okuno28 <67859853+Okuno28@users.noreply.github.com>
2020-07-11 16:13:10 +0900

    再提出　-　Javaの基礎

--- basic/src/basic/Task2.java
+++ basic/src/basic/Task2.java
@@ -3,7 +3,8 @@ package basic;
 public class Task2 {
 
 	public static void main(String[] args) {
-		//コメント
+		/*/コメント
+		コメント */
 		System.out.println("hogehoge");
 		System.out.print("hogehoge2");
 	}
