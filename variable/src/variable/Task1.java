e74ca7097a834e45c49477d52b3512898d269324
Okuno28 <67859853+Okuno28@users.noreply.github.com>
2020-07-11 15:54:48 +0900

    課題提出　-　変数・定数

---
+++ variable/src/variable/Task1.java
@@ -0,0 +1,19 @@
+package variable;
+
+public class Task1 {
+
+	public static void main(String[] args) {
+		double x=31.2;
+		String a="Java研修";
+		int b=9;
+		long c=2147483648L;
+		char d='才';
+		System.out.println(x);
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+
+	}
+
+}
---
+++ variable/src/variable/Task2.java
@@ -0,0 +1,12 @@
+package variable;
+
+public class Task2 {
+
+	public static void main(String[] args) {
+		String name="奥野　早貴";
+		final String NUMBER="080-5670-5391";
+		System.out.println(name + "です。");
+		System.out.println("私の電話番号は\'" + NUMBER + "\'です。");
+	}
+
+}
