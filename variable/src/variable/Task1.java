edcccc1c2485f546408ce203f39a3e138f7d0e57
Okuno28 <67859853+Okuno28@users.noreply.github.com>
2020-07-18 17:56:50 +0900

    再提出　-　変数・定数

--- variable/src/variable/Task1.java
+++ variable/src/variable/Task1.java
@@ -3,11 +3,11 @@ package variable;
 public class Task1 {
 
 	public static void main(String[] args) {
-		double x=31.2;
-		String a="Java研修";
-		int b=9;
-		long c=2147483648L;
-		char d='才';
+		double x = 31.2;
+		String a = "Java研修";
+		int b = 9;
+		long c = 2147483648L;
+		char d = '才';
 		System.out.println(x);
 		System.out.println(a);
 		System.out.println(b);
